ebbf0acfc2442cb8633458bc77702b936d0f1892
chayennefelix <113393167+chayennefelix@users.noreply.github.com>
2022-11-04 19:06:09 -0300

    Add files via upload

---
+++ aula01.js
@@ -0,0 +1,15 @@
+const msg = new Promise ((resolve, reject)=>{
+    try{
+       if(Math.random() >= 0.5) {
+           resolve()
+       }
+       else{
+           reject()
+       }
+    }
+    catch(e){}
+})
+
+msg
+.then((parametros)=>console.log("Resposta Correta"))
+.catch((erro)=>console.log ("Resposta Errada"))
